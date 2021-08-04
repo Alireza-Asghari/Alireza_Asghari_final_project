9bb8c4700fe18f8eb275bad8db7efaa488212277
Alireza <asgharialireza1995@gmail.com>
2021-08-04 16:53:14 +0430

    adding new-title route

--- src/routes/main.jsx
+++ src/routes/main.jsx
@@ -1,6 +1,7 @@
 import HomePage from '../components/HomePage'
 import BestSelling from '../components/BestSelling/BestSelling'
 import NewTitle from '../components/NewTitle/NewTitle'
+import NewTitleSinglePage from '../components/NewTitle/NewTitleSinglePage'
 import Criminal from '../components/criminal/criminal'
 import Horror from '../components/horror/horror'
 import HorrorSinglePage from '../components/horror/HorroSinglePage'
@@ -12,12 +13,13 @@ import CriminalSinglePage from '../components/criminal/CriminalSinglePage'
 import FictionSinglePage from '../components/fiction/FictionSinglePage'
 
 export const routes = [
-    
+
     { path: '/Home-page', exact: true, component: HomePage },
     { path: '/best-selling/:id', exact: true, component: ItemContentPage },
     { path: '/best-selling', exact: true, component: BestSelling },
     { path: '/login/:id', exact: true, component: Basket },
-    { path: '/Newtitle', exact: true, component: NewTitle },
+    { path: '/new-title', exact: true, component: NewTitle },
+    { path: '/new-title/:id', exact: true, component: NewTitleSinglePage },
     { path: '/criminal', exact: true, component: Criminal },
     { path: '/criminal/:id', exact: true, component: CriminalSinglePage },
     { path: '/horror', exact: true, component: Horror },
