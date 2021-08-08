67d5a13ae2955805442fdfa8b511b8064c6d32ab
Alireza <asgharialireza1995@gmail.com>
2021-08-09 01:17:33 +0430

    making the userInfo page, respinsive

--- src/pages/userInfo.jsx
+++ src/pages/userInfo.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+import {FaUserPlus} from 'react-icons/fa'
+import Header from '../components/HomePage.header/header'
+
 
 const UserInfo = () => {
     return (
         <>
-            
+        <Header/>
+            <div className='main-box-userInfo '>
+                <div className='row h-100 '>
+                    <div className='col-12 col-md-6 text-center P-2 log-in'>
+                        <div className="log-in-box">
+                            <div className='log-in-content d-flex flex-column'>
+                                <div className=' p-3'>
+                                    <FaSignInAlt color='grey' size="50px"/>
+                                </div>
+                                <div className='p-3 entre-text'>
+                                    عضو سایت هستید؟
+                                </div>
+                                <div className='p-3 text-confirmation'>
+                                    برای تکمیل فرآیند خرید خود وارد شوید.
+                                </div>
+                                <div className='p-3'>
+                                    <button className='p-2 px-5 btn btn-primary entre-but w-75'>ورود</button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                
+
+                
+                    <div className='col-12 col-md-6 text-center P-2 log-in'>
+                        <div className="log-in-box">
+                            <div className='log-in-content d-flex flex-column'>
+                                <div className=' p-3'>
+                                    <FaUserPlus color="grey" size="50px"/>
+                                </div>
+                                <div className='p-3 entre-text'>
+                                    ثبت نام
+                                </div>
+                                <div className='p-3 text-confirmation'>
+                                    برای تکمیل فرآیند خرید خود ثبت نام شوید.
+                                </div>
+                                <div className='p-3'>
+                                    <button className='p-2 px-5 btn btn-success entre-but w-75'>ثبت نام</button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
